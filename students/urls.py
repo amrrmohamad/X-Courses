15cce1b1fb98c8b442cf954e414268171eb1ac40
amrrmohamad <amr.r.mohamad55@gmail.com>
2024-09-25 19:21:55 +0300

    update

--- students/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import course_list, view_profile , edit_profile
-
-urlpatterns = [
-    path('courses/', course_list, name='course_list'),
-    path('profile/', view_profile, name='view_profile'),
-    path('profile/edit/', edit_profile, name='edit_profile'),
-]
